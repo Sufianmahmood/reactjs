e2ec734a3d41186379c91beb68158ed905807d0b
hassanmahmoodjafri121 <100502783+Sufianmahmood@users.noreply.github.com>
2024-11-09 15:42:24 +0530

    projectreact

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,11 +3,11 @@ import Chai from "./chai"
 Chai
 
 function App() {
-
+ const username = "chai aur code"
   return (
 <>
   <Chai/>
-  <h1>Superman drinks chai </h1>
+  <h1>Superman drinks chai {username} </h1>
   <p>Its tasty</p>
 </>  
   )
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+ const [counter, setCounter] =  useState(15)
+
+  //let counter = 15
+  
+  const addValue = () => {
+    if (counter < 25) {
+      console.log("value added", counter);
+      setCounter(counter + 1);
+    } else {
+      console.log("Counter has reached 25, cannot add further");
+    }
+  };
+  
+  const removeValue = () => {
+    if (counter > 0) {
+      console.log("value subtracted");
+      setCounter(counter - 1);
+    } else {
+      console.log("Counter has reached 0, cannot subtract further");
+    }
+  };
+
+ return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value : {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add value</button>
+     <br /><br />
+      <button
+      on onClick={removeValue}
+      >Remove</button>
+    </>
+  )
+}
+
+export default App

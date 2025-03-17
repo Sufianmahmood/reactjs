790d537b66461c935b5dbaed3b7fb305ffda941b
hassanmahmoodjafri121 <100502783+Sufianmahmood@users.noreply.github.com>
2025-03-18 01:17:12 +0530

    working on project

--- 12MegaBlog/src/components/Login.jsx
+++ 12MegaBlog/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {link, useNavigate} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import { login as authLogin } from '../store/authSlice'
 import {Button, Input, Logo} from "./index"
 import {useDispatch} from 'react-redux'
@@ -7,16 +7,16 @@ import AuthService from '../appwrite/auth'
 import {useForm} from 'react-hook-form'
 
 
-function Login() {
+function Login() { 
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const {register, handleSubmit} = useForm()
   const [error, setError] = useState("")
 
-  const login = async (data) => {
+   const login = async (data) => {
     setError("")
     try {
-      const response =authLogin(data)
+      const session = await AuthService.login(data)
       if(session) {
         const userData = await AuthService.getCurrentUser()
         if(userData) dispatch(authLogin(userData))
@@ -25,7 +25,7 @@ function Login() {
     }catch(error) {
       setError(error.message)
   }
-
+ }
   return (
     <div
     className='flex items-center justify-center w-full'

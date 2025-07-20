ce0d725ab0ec0887de1e3f03320e50b10cf5d262
hassanmahmoodjafri121 <100502783+Sufianmahmood@users.noreply.github.com>
2025-07-20 17:56:11 +0530

    working

---
+++ 12MegaBlog/src/components/Select.jsx
@@ -0,0 +1,32 @@
+import React, { useId } from 'react';
+
+const Select = React.forwardRef(function Select(
+  { options = [], label, className = '', ...props },
+  ref
+) {
+  const id = useId();
+
+  return (
+    <div className="w-full">
+      {label && (
+        <label htmlFor={id} className="block mb-1 text-sm font-medium">
+          {label}
+        </label>
+      )}
+      <select
+        {...props}
+        id={id}
+        ref={ref}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+      >
+        {options.map((option, index) => (
+          <option key={index} value={option.value || option}>
+            {option.label || option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+});
+
+export default Select;

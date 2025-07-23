ceb8b607377d928a68661e97adaaf9409d455dd3
hassanmahmoodjafri121 <100502783+Sufianmahmood@users.noreply.github.com>
2025-07-23 20:05:00 +0530

    working

---
+++ 12MegaBlog/src/components/post-form/PostForm.jsx
@@ -0,0 +1,17 @@
+import React, {useCallback} from 'react';
+import { useForm } from 'react-hook-form';   
+import {Button, Input, Select, RTE} from  '../index';
+import appwriteService from "../../appwrite/config";
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';  
+
+function PostForm() {
+    const {register, handleSubmit, watch,}
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostForm
